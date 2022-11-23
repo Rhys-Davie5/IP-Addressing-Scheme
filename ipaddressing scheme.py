581e4330113d2b15affa9a947eb4208c7fdf0123
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-11-23 15:14:35 +0000

    2

--- ipaddressing scheme.py
+++ ipaddressing scheme.py
@@ -1,7 +1,9 @@
 Number_of_subnets = [1,   2,   4,  8,  16, 32, 64, 128, 256]
 Number_of_hosts =   [256, 128, 64, 32, 16, 8,  4,  2,   1]
-Number_of_subnet_mask = ["/24","/25","/26","/27","/28","/29",]
+Number_of_subnet_mask = ["/24","/25","/26","/27","/28","/29","/30","/31","/32"]
+
 NetworkID = input("Please enter the Network address ")
+PreFix_Length = input("Enter the Prefix length of the network")
 
 
 
