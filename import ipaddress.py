d24f318a603fd8996192fb56992c1cc2e82686f1
Rhys Davies <88878657+Rhys-Davie5@users.noreply.github.com>
2022-12-04 19:56:35 +0000

    Delete import ipaddress.py

--- import ipaddress.py
+++
@@ -1,8 +0,0 @@
-import ipaddress
-
-# Create a network object for the IP address range
-network = ipaddress.ip_network('192.0.0.0/24')
-
-# Iterate over the addresses in the network
-for address in network:
-  print(address)
