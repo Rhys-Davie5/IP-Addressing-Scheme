a2f4f8de8c08c06e9001eb96238b7314ba09d389
Rhys Davies <88878657+Rhys-Davie5@users.noreply.github.com>
2023-10-01 16:19:37 +0100

    Updated code

--- lol.py
+++ lol.py
@@ -1,12 +1,19 @@
+# Import the ipaddress module
 import ipaddress
 
-# Create an IPv4 network
-ipv4_net = ipaddress.IPv4Network('192.0.0.0/24')
+# Define the network ID
+network_id = '192.168.0.0/24'
 
-# Divide the network into 3 subnets
-subnets = list(ipv4_net.subnets(new_prefix=26))
+# Create an IPv4Network object from the network ID
+network = ipaddress.IPv4Network(network_id)
 
-# Print the subnet addresses
-for subnet in subnets:
-    print(subnet)
+# Print the network ID and subnet mask
+print(f'Network ID: {network.network_address}')
+print(f'Subnet Mask: {network.netmask}')
 
+# Print the first and last usable IP addresses in the network
+print(f'First Usable IP Address: {network.network_address + 1}')
+print(f'Last Usable IP Address: {network.broadcast_address - 1}')
+
+# Print the total number of usable IP addresses in the network
+print(f'Total Usable IP Addresses: {network.num_addresses - 2}')
